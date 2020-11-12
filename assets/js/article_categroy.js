6db7d082029f3aee41bfe3bce8c98aa16c97bb81
Qin-gl <truelovegirls@163.com>
2020-11-12 17:53:43 +0800

    完成了文章管理的分类列表的编辑和删除功能

--- assets/js/article_categroy.js
+++ assets/js/article_categroy.js
@@ -1,82 +1,131 @@
 /*
  * @Author: your name
  * @Date: 2020-11-10 20:11:19
- * @LastEditTime: 2020-11-11 23:52:17
+ * @LastEditTime: 2020-11-12 17:45:16
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \bigNews\assets\js\article_categroy.js
  */
 $(function() {
+    let addIndex = null
+    let editIndex = null
+    let renderData
+
     function getCateList() {
         layui.use('table', function() {
-            var table = layui.table;
 
-            //第一个实例
-            table.render({
-                elem: '#myTable',
-                height: 500,
-                url: '/my/article/cates',
-                parseData: function(res) {
-                    return {
-                        "code": res.status,
-                        "msg": res.message,
-                        "data": res.data
-                    }
-                },
+                //第一个实例
+                layui.table.render({
+                    elem: '#myTable',
+                    height: 500,
+                    url: '/my/article/cates',
+                    request: {
+                        pageName: 'curr' //页码的参数名称，默认：page
+                            ,
+                        limitName: 'nums' //每页数据量的参数名，默认：limit
+                    },
+                    parseData: function(res) {
+                        return {
+                            "code": res.status,
+                            "msg": res.message,
+                            "data": res.data
+                        }
+                    },
 
-                cols: [
-                    [ //表头
-                        { field: 'name', title: '分类名称' },
-                        { field: 'alias', title: '分类别名' },
-                        { field: 'is_delete', title: '操作', templet: '#titleTpl' },
-
+                    cols: [
+                        [ //表头
+                            { field: 'Id', title: 'ID', hide: 'false' },
+                            { field: 'name', title: '分类名称' },
+                            { field: 'alias', title: '分类别名' },
+                            { field: 'is_delete', title: '操作', toolbar: '#titleTpl' },
+                        ]
                     ]
-                ]
+                })
             })
+            //监听工具条 
+        layui.table.on('tool(myTable)', function(res) {
+            let data = res.data
+            let layEvent = res.event
+                // let tr = res.tr
+                // console.log(data);
+            if (layEvent === 'edit') {
+                console.log('我被编辑了');
+                editIndex = layer.open({
+                    type: 1,
+                    title: '修改文章分类',
+                    content: $('#editCategroy').html(),
+                    area: ['500px', '250px']
+                })
+                $.get(`/my/article/cates/${data.Id}`, function(res) {
+                        console.log(res);
+                        if (res.status == 0) {
+                            renderData = res.data
+                            layui.form.val('editForm', res.data)
+                            console.log($(this));
+
+                        }
+                    })
+                    // 编辑更新功能
+                $('.myEditForm').on('submit', function(e) {
+                    e.preventDefault()
+                    $.post('/my/article/updatecate', $(this).serialize(), function(res) {
+                        console.log(res)
+                        layer.msg(res.message)
+                        layer.close(editIndex)
+                        getCateList()
+                    })
+                })
+                $('.reset').on('click', function(e) {
+                        e.preventDefault()
+                        layui.form.val('editForm', renderData)
+                    })
+                    // 删除分类功能
+            } else if (layEvent === 'del') {
+                console.log('我被删除了');
+                layer.confirm('是否删除该分类?', { icon: 3, title: '提示' }, function(index) {
+                    $.get(`/my/article/deletecate/${data.Id}`, function(res) {
+                        if (res.status == 0) {
+                            layer.msg(res.message)
+                            getCateList()
+                        }
+                    })
+
+                    layer.close(index);
+                });
+
+            }
         })
     }
     getCateList()
-    let addIndex = null
-    let editIndex = null
-        //弹出添加分类框
+
+    //弹出添加分类框
     $('.addCategroy').on('click', function() {
             addIndex = layer.open({
-                type: 1,
-                title: '添加文章分类',
-                content: $('#addCategroy').html(),
-                area: ['500px', '250px']
-            })
-            console.log($('.myform input[name=catename]'));
+                    type: 1,
+                    title: '添加文章分类',
+                    content: $('#addCategroy').html(),
+                    area: ['500px', '250px']
+                })
+                // console.log($('.myform input[name=catename]'));
 
         })
         //添加分类
     $('body').on('submit', '.myform', function(e) {
-            e.preventDefault()
-            $.ajax({
-                type: 'POST',
-                url: '/my/article/addcates',
-                data: $(this).serialize(),
-                success: function(res) {
-                    console.log(res);
+        e.preventDefault()
+        $.ajax({
+            type: 'POST',
+            url: '/my/article/addcates',
+            data: $(this).serialize(),
+            success: function(res) {
+                console.log(res);
 
-                    layer.msg(res.message)
-                    layer.close(addIndex)
-                    getCateList()
-                }
-            })
+                layer.msg(res.message)
+                layer.close(addIndex)
+                getCateList()
+            }
         })
-        //弹出修改分类框
-    $('body').on('click', '.editBtn', function() {
-            editIndex = layer.open({
-                type: 1,
-                title: '修改文章分类',
-                content: $('#editCategroy').html(),
-                area: ['500px', '250px']
-            })
-            $.get('')
-        })
-        //获取分类
-        //修改分类
+    })
+
     $('body').on('click', 'myEditForm', function(e) {
         e.preventDefault()
         $.post('/my/article/updatecate', $(this).serialize(), function(res) {
