2465af1ba3b9fa9cb8fc261bd46f5284ef436894
Qin-gl <truelovegirls@163.com>
2020-11-13 20:01:00 +0800

    完成分页功能

--- assets/js/article_list.js
+++ assets/js/article_list.js
@@ -3,14 +3,14 @@
 /*
  * @Author: your name
  * @Date: 2020-11-12 17:57:55
- * @LastEditTime: 2020-11-13 17:56:37
+ * @LastEditTime: 2020-11-13 19:03:48
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \bigNews\assets\js\article_list.js
  */
 $(function() {
     getCategory()
-
+        // 下拉框分类渲染
     function getCategory() {
         $.get('/my/article/cates', function(res) {
             console.log(res);
@@ -30,15 +30,15 @@ $(function() {
         })
     }
     getArticleList()
-
+        // 文章列表渲染
     function getArticleList() {
         layui.use('table', function() {
-            //第一个实例
+            //table实例
             layui.table.render({
                 elem: '#myTable',
                 height: 312,
-                url: '/my/article/list', //数据接口
-                parseData: function(res) {
+                url: '/my/article/list',
+                parseData: function(res) { //格式化返回数据
                     return {
                         "code": res.status,
                         "msg": res.message,
@@ -47,6 +47,8 @@ $(function() {
                     }
                 },
                 page: true, //开启分页
+                limit: 4,
+                limits: [4, 8, 12],
                 request: {
                     pageName: 'pagenum',
                     limitName: 'pagesize'
@@ -68,23 +70,37 @@ $(function() {
     }
     // 监听tooltar工具
     layui.table.on('tool(myTable)', function(res) {
-        let data = res.data
-        let layEvent = res.event
-        console.log(data)
-        if (layEvent === 'edit') {
-            layer.msg('还在写')
-        } else if (layEvent === 'del') {
-            layer.confirm('是否删除该文章？', {
-                icon: 3,
-                title: '提示',
-            }, function(i) {
-                $.get(`/my/article/delete/${data.Id}`, function(info) {
-                    layer.msg(info.message)
-                    layer.close(i)
-                    getArticleList()
+            let data = res.data
+            let layEvent = res.event
+            console.log(data)
+            if (layEvent === 'edit') {
+                layer.msg('还在写')
+            } else if (layEvent === 'del') {
+                layer.confirm('是否删除该文章？', {
+                    icon: 3,
+                    title: '提示',
+                }, function(i) {
+                    $.get(`/my/article/delete/${data.Id}`, function(info) {
+                        layer.msg(info.message)
+                        layer.close(i)
+                        getArticleList()
 
+                    })
                 })
-            })
-        }
-    })
+            }
+        })
+        //点击筛选分类重新渲染
+    layui.form.on('select', function(data) {
+        // console.log(data.elem); //得到select原始DOM对象
+        // console.log(data.value); //得到被选中的值
+        // console.log(data.othis); //得到美化后的DOM对象
+        $('.myform').on('submit', function(e) {
+            e.preventDefault()
+            console.log(data.value);
+            console.log(data.elem)
+            console.log(data.othis)
+
+        })
+    });
+
 })
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-11-09 18:02:10
- * @LastEditTime: 2020-11-13 10:09:44
+ * @LastEditTime: 2020-11-13 18:42:21
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \bigNews\assets\js\utils.js
@@ -12,7 +12,7 @@ $.ajaxPrefilter(function(options) {
     options.url = 'http://ajax.frontend.itheima.net' + options.url
     if (options.url.includes('/my')) {
         options.headers = {
-            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjIyMjEsInVzZXJuYW1lIjoicWdsIiwicGFzc3dvcmQiOiIiLCJuaWNrbmFtZSI6IlFnbCIsImVtYWlsIjoiMTIzMTQyM2VycXdAMTYzLmNvbSIsInVzZXJfcGljIjoiIiwiaWF0IjoxNjA1MjI3NTUzLCJleHAiOjE2MDUyNjM1NTN9.alX2S7GINR9j_0aJ9nWFN9hVYREUzqC95qV2Y6605gM"
+            Authorization: localStorage.getItem('token')
                 // localStorage.getItem('token')
         }
     }
